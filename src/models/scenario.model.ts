b98a0c322984494476a43d48a2d64ec25c9117af
Adrian Segade <adrian.segade@learnovatecentre.org>
2019-11-27 15:36:27 +0000

    Updated model
    Sorted fields differently

--- src/models/scenario.model.ts
+++ src/models/scenario.model.ts
@@ -2,60 +2,60 @@ import {Entity, model, property} from '@loopback/repository';
 
 @model()
 export class Scenario extends Entity {
-	@property({
-		type: 'number',
-		id: true,
-		generated: true,
-	})
-	id?: number;
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
 
-	@property({
-		type: 'number',
-		required: true,
-	})
-	product: number;
+  @property({
+    type: 'number',
+    required: true,
+  })
+  product: number;
 
-	@property({
-		type: 'number',
-		required: true,
-	})
-	interest: number;
+  @property({
+    type: 'number',
+    required: true,
+  })
+  interest: number;
 
-	@property({
-		type: 'string',
-		required: true,
-	})
-	name: string;
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
 
-	@property({
-		type: 'string',
-		required: true,
-	})
-	text: string;
+  @property({
+    type: 'string',
+    required: true,
+  })
+  text: string;
 
-	@property({
-		type: 'string',
-	})
-	description?: string;
+  @property({
+    type: 'number',
+    required: true,
+  })
+  level: number;
 
-	@property({
-		type: 'number',
-		required: true,
-	})
-	level: number;
+  @property({
+    type: 'string',
+  })
+  resource?: string;
 
-	@property({
-		type: 'string',
-	})
-	resource?: string;
+  @property({
+    type: 'string',
+  })
+  description?: string;
 
-	constructor(data?: Partial<Scenario>) {
-		super(data);
-	}
+  constructor(data?: Partial<Scenario>) {
+    super(data);
+  }
 }
 
 export interface ScenarioRelations {
-	// describe navigational properties here
+  // describe navigational properties here
 }
 
 export type ScenarioWithRelations = Scenario & ScenarioRelations;
