c9cfce27eb16e1d6a539b76acee4d33e631935c6
Adrian Segade <adrian.segade@learnovatecentre.org>
2019-12-09 13:00:03 +0000

    Created category model, repository and controller

---
+++ src/models/category.model.ts
@@ -0,0 +1,43 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Category extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+  })
+  text?: string;
+
+  @property({
+    type: 'string',
+  })
+  resource?: string;
+
+  @property({
+    type: 'string',
+  })
+  description?: string;
+
+
+  constructor(data?: Partial<Category>) {
+    super(data);
+  }
+}
+
+export interface CategoryRelations {
+  // describe navigational properties here
+}
+
+export type CategoryWithRelations = Category & CategoryRelations;
