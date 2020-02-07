037d6cf5c57d1d2b77b9f23325f9616a470dd76d
Adrian Segade <adrian.segade@learnovatecentre.org>
2020-02-07 12:05:21 +0000

    Added fields to course
    Fields added to fill the new course-screen in the client

--- src/controllers/course.controller.ts
+++ src/controllers/course.controller.ts
@@ -1,24 +1,5 @@
-import {
-  Count,
-  CountSchema,
-  Filter,
-  repository,
-  Where,
-  FilterBuilder,
-  WhereBuilder,
-} from '@loopback/repository';
-import {
-  post,
-  param,
-  get,
-  getFilterSchemaFor,
-  getModelSchemaRef,
-  getWhereSchemaFor,
-  patch,
-  put,
-  del,
-  requestBody,
-} from '@loopback/rest';
+import {Filter, repository, FilterBuilder} from '@loopback/repository';
+import {param, get, getModelSchemaRef} from '@loopback/rest';
 import {Course} from '../models';
 import {CourseRepository, ProductRepository} from '../repositories';
 import {CommonController} from './common.controller';
@@ -33,44 +14,6 @@ export class CourseController {
   ) {
     this.commonController = new CommonController(productRepository);
   }
-
-  // @post('/courses', {
-  //   responses: {
-  //     '200': {
-  //       description: 'Course model instance',
-  //       content: {'application/json': {schema: getModelSchemaRef(Course)}},
-  //     },
-  //   },
-  // })
-  // async create(
-  //   @requestBody({
-  //     content: {
-  //       'application/json': {
-  //         schema: getModelSchemaRef(Course, {
-  //           title: 'NewCourse',
-  //           exclude: ['id'],
-  //         }),
-  //       },
-  //     },
-  //   })
-  //   course: Omit<Course, 'id'>,
-  // ): Promise<Course> {
-  //   return this.courseRepository.create(course);
-  // }
-
-  // @get('/courses/count', {
-  //   responses: {
-  //     '200': {
-  //       description: 'Course model count',
-  //       content: {'application/json': {schema: CountSchema}},
-  //     },
-  //   },
-  // })
-  // async count(
-  //   @param.query.object('where', getWhereSchemaFor(Course)) where?: Where<Course>,
-  // ): Promise<Count> {
-  //   return this.courseRepository.count(where);
-  // }
 
   @get('/{productname}/courses', {
     responses: {
@@ -102,86 +45,6 @@ export class CourseController {
     return this.courseRepository.find(filter);
   }
 
-  // @patch('/courses', {
-  //   responses: {
-  //     '200': {
-  //       description: 'Course PATCH success count',
-  //       content: {'application/json': {schema: CountSchema}},
-  //     },
-  //   },
-  // })
-  // async updateAll(
-  //   @requestBody({
-  //     content: {
-  //       'application/json': {
-  //         schema: getModelSchemaRef(Course, {partial: true}),
-  //       },
-  //     },
-  //   })
-  //   course: Course,
-  //   @param.query.object('where', getWhereSchemaFor(Course)) where?: Where<Course>,
-  // ): Promise<Count> {
-  //   return this.courseRepository.updateAll(course, where);
-  // }
-
-  // @get('/courses/{id}', {
-  //   responses: {
-  //     '200': {
-  //       description: 'Course model instance',
-  //       content: {'application/json': {schema: getModelSchemaRef(Course)}},
-  //     },
-  //   },
-  // })
-  // async findById(@param.path.number('id') id: number): Promise<Course> {
-  //   return this.courseRepository.findById(id);
-  // }
-
-  // @patch('/courses/{id}', {
-  //   responses: {
-  //     '204': {
-  //       description: 'Course PATCH success',
-  //     },
-  //   },
-  // })
-  // async updateById(
-  //   @param.path.number('id') id: number,
-  //   @requestBody({
-  //     content: {
-  //       'application/json': {
-  //         schema: getModelSchemaRef(Course, {partial: true}),
-  //       },
-  //     },
-  //   })
-  //   course: Course,
-  // ): Promise<void> {
-  //   await this.courseRepository.updateById(id, course);
-  // }
-
-  // @put('/courses/{id}', {
-  //   responses: {
-  //     '204': {
-  //       description: 'Course PUT success',
-  //     },
-  //   },
-  // })
-  // async replaceById(
-  //   @param.path.number('id') id: number,
-  //   @requestBody() course: Course,
-  // ): Promise<void> {
-  //   await this.courseRepository.replaceById(id, course);
-  // }
-
-  // @del('/courses/{id}', {
-  //   responses: {
-  //     '204': {
-  //       description: 'Course DELETE success',
-  //     },
-  //   },
-  // })
-  // async deleteById(@param.path.number('id') id: number): Promise<void> {
-  //   await this.courseRepository.deleteById(id);
-  // }
-
   createFilter(
     productid: number,
     literacyLvl: number,
@@ -197,17 +60,27 @@ export class CourseController {
         name: true,
         title: true,
         description: true,
+        text: true,
         level: true,
         skill: true,
         location: true,
-        date: true,
+        date_start: true,
+        date_finish: true,
+        duration: true,
+        frequency: true,
+        address: true,
         link: true,
+        enrolment_period_start: true,
+        enrolment_period_finish: true,
+        contact_person: true,
+        contact_telephone: true,
+        contact_email: true,
       })
       .offset(0)
       .order('id ASC')
       .where({
         product: productid,
-        date: {
+        enrolment_period_finish: {
           gte: new Date().toString(),
         },
         location,
